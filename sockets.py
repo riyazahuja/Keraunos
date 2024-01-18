20e75b613d80cde28921c0763c885441c96da55c
Arturo Paras <aparas@andrew.cmu.edu>
2024-01-18 00:09:29 -0500

    custom color template, only draw biggest bounding box

--- sockets.py
+++ sockets.py
@@ -34,12 +34,15 @@ def capture_app_window(app_title):
             hsvFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
             imageFrame=hsvFrame
 
+            # # Set range for custom color and define mask
+            # custom_lower = np.array([_, _, _], np.uint8)
+            # custom_upper = np.array([_, _, _], np.uint8)
+            # custom_mask = cv2.inRange(hsvFrame, custom_lower, custom_upper)
+
             # Set range for red color and define mask
             red_lower = np.array([136, 87, 111], np.uint8)
             red_upper = np.array([180, 255, 255], np.uint8)
             red_mask = cv2.inRange(hsvFrame, red_lower, red_upper)
-
-
         
             # Set range for green color and  
             # define mask 
@@ -58,6 +61,11 @@ def capture_app_window(app_title):
             # between imageFrame and mask determines 
             # to detect only that particular color 
             kernel = np.ones((5, 5), "uint8") 
+
+            # # For custom color 
+            # custom_mask = cv2.dilate(custom_mask, kernel) 
+            # res_custom = cv2.bitwise_and(imageFrame, imageFrame,  
+            #                         mask = custom_mask) 
             
             # For red color 
             red_mask = cv2.dilate(red_mask, kernel) 
@@ -73,56 +81,89 @@ def capture_app_window(app_title):
             blue_mask = cv2.dilate(blue_mask, kernel) 
             res_blue = cv2.bitwise_and(imageFrame, imageFrame, 
                                     mask = blue_mask) 
+            
+            # # Creating contour to track custom color 
+            # contours, hierarchy = cv2.findContours(custom_mask, 
+            #                                     cv2.RETR_TREE, 
+            #                                     cv2.CHAIN_APPROX_SIMPLE) 
+            
+            # for pic, contour in enumerate(contours): 
+            #     area = cv2.contourArea(contour) 
+            #     if(area > 900): 
+            #         x, y, w, h = cv2.boundingRect(contour) 
+            #         imageFrame = cv2.rectangle(imageFrame, (x, y),  
+            #                                 (x + w, y + h),  
+            #                                 (_, _, _), 2) 
+                    
+            #         cv2.putText(imageFrame, "Red Colour", (x, y), 
+            #                     cv2.FONT_HERSHEY_SIMPLEX, 1.0, 
+            #                     (_, _, _))     
         
             # Creating contour to track red color 
             contours, hierarchy = cv2.findContours(red_mask, 
                                                 cv2.RETR_TREE, 
                                                 cv2.CHAIN_APPROX_SIMPLE) 
             
+            maxArea = None
+            maxContour = None
             for pic, contour in enumerate(contours): 
                 area = cv2.contourArea(contour) 
-                if(area > 900): 
-                    x, y, w, h = cv2.boundingRect(contour) 
-                    imageFrame = cv2.rectangle(imageFrame, (x, y),  
-                                            (x + w, y + h),  
-                                            (0, 0, 255), 2) 
-                    
-                    cv2.putText(imageFrame, "Red Colour", (x, y), 
-                                cv2.FONT_HERSHEY_SIMPLEX, 1.0, 
-                                (0, 0, 255))     
+                if(area > 900) and (maxArea is None or area > maxArea):
+                    maxArea = area
+                    maxContour = contour
+            if(maxContour is not None):
+                x, y, w, h = cv2.boundingRect(maxContour) 
+                imageFrame = cv2.rectangle(imageFrame, (x, y),  
+                                        (x + w, y + h),  
+                                        (0, 0, 255), 2) 
+                
+                cv2.putText(imageFrame, "Red Colour", (x, y), 
+                            cv2.FONT_HERSHEY_SIMPLEX, 1.0, 
+                            (0, 0, 255))     
         
             # Creating contour to track green color 
             contours, hierarchy = cv2.findContours(green_mask, 
                                                 cv2.RETR_TREE, 
                                                 cv2.CHAIN_APPROX_SIMPLE) 
             
+            maxArea = None
+            maxContour = None
             for pic, contour in enumerate(contours): 
                 area = cv2.contourArea(contour) 
-                if(area > 900): 
-                    x, y, w, h = cv2.boundingRect(contour) 
-                    imageFrame = cv2.rectangle(imageFrame, (x, y),  
-                                            (x + w, y + h), 
-                                            (0, 255, 0), 2) 
-                    
-                    cv2.putText(imageFrame, "Green Colour", (x, y), 
-                                cv2.FONT_HERSHEY_SIMPLEX,  
-                                1.0, (0, 255, 0)) 
+                if(area > 900) and (maxArea is None or area > maxArea):
+                    maxArea = area
+                    maxContour = contour
+            if(maxContour is not None):
+                x, y, w, h = cv2.boundingRect(maxContour) 
+                imageFrame = cv2.rectangle(imageFrame, (x, y),  
+                                        (x + w, y + h),  
+                                        (0, 255, 0), 2) 
+                
+                cv2.putText(imageFrame, "Green Colour", (x, y), 
+                            cv2.FONT_HERSHEY_SIMPLEX, 1.0, 
+                            (0, 255, 0))     
         
             # Creating contour to track blue color 
             contours, hierarchy = cv2.findContours(blue_mask, 
                                                 cv2.RETR_TREE, 
                                                 cv2.CHAIN_APPROX_SIMPLE) 
+            
+            maxArea = None
+            maxContour = None
             for pic, contour in enumerate(contours): 
                 area = cv2.contourArea(contour) 
-                if(area > 900): 
-                    x, y, w, h = cv2.boundingRect(contour) 
-                    imageFrame = cv2.rectangle(imageFrame, (x, y), 
-                                            (x + w, y + h), 
-                                            (255, 0, 0), 2) 
-                    
-                    cv2.putText(imageFrame, "Blue Colour", (x, y), 
-                                cv2.FONT_HERSHEY_SIMPLEX, 
-                                1.0, (255, 0, 0)) 
+                if(area > 900) and (maxArea is None or area > maxArea):
+                    maxArea = area
+                    maxContour = contour
+            if(maxContour is not None):
+                x, y, w, h = cv2.boundingRect(maxContour) 
+                imageFrame = cv2.rectangle(imageFrame, (x, y),  
+                                        (x + w, y + h),  
+                                        (255, 0, 0), 2) 
+                
+                cv2.putText(imageFrame, "Blue Colour", (x, y), 
+                            cv2.FONT_HERSHEY_SIMPLEX, 1.0, 
+                            (255, 0, 0))     
                     
             # Program Termination 
             cv2.imshow("Multiple Color Detection in Real-Time", imageFrame) 
